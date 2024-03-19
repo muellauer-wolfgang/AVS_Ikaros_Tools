da1e2269771309df7c864c6e1c8e8dcab89d9eed
wolfgang muellauer <w.muellauer@mdc.at>
2024-03-19 23:00:10 +0100

    New Tool Copy_Content_doc_Json für Import der Fat_Files

---
+++ src/Copy_Content_dot_Json/Infrastructure/ConfigProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Copy_Content_dot_Json.Infrastructure.Interfaces;
+
+namespace Copy_Content_dot_Json.Infrastructure
+{
+  public class ConfigProvider : IConfigProvider
+  {
+    //public string BasePath => @"H:\tmp\AVS\Export_Documents";
+    public string BasePath => @"F:\Documents\Export_Documents";
+
+    //public string ExportPath => @"H:\tmp\AVS\Fat_Documents";
+    public string ExportPath => @"F:\Documents\Fat_Documents";
+
+  } //end   public class ConfigProvider : IConfigProvider
+
+} //end namespace Copy_Content_dot_Json.Infrastructure
+
+
---
+++ src/Copy_Content_dot_Json/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Copy_Content_dot_Json.Infrastructure.Interfaces;
+using Copy_Content_dot_Json.Infrastructure;
+
+namespace Copy_Content_dot_Json
+{
+  internal class Program
+  {
+    public static IConfigProvider _config = new ConfigProvider();
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Copy_Content_dot_Json -- Kopieren der zugehörigen content.json Files");
+      var allFatDirectories = Directory.GetDirectories(_config.ExportPath);
+      foreach (string fatDir in allFatDirectories) {
+        string sourceFile = Path.Combine(fatDir.Replace(_config.ExportPath, _config.BasePath), "content.json");
+        File.Copy(sourceFile, Path.Combine(fatDir, "content.json"));
+        Console.WriteLine($"Dest Dir: {fatDir}");
+      }
+
+    } //end     static void Main(string[] args)
+
+  } //end   internal class Program
+
+} //end namespace Copy_Content_dot_Json
+
+
+
+
+
+
